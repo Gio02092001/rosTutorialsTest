c362f205768fbc3f48be067702fbef817638ea70
Gio02092001 <giovannicarlucci9@yahoo.de>
2024-11-12 00:51:46 +0100

    C++ and Python sub und pub work but only with catkin_make

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/gio/rosTutorialsTest/src
+# Build directory: /home/gio/rosTutorialsTest/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("beginner_tutorials")
---
+++ build/beginner_tutorials/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/gio/rosTutorialsTest/src/beginner_tutorials
+# Build directory: /home/gio/rosTutorialsTest/build/beginner_tutorials
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
